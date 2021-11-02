000284258ba47f275c1832e739cc4df9df5aae2d
LI_XE <hyangeun312@gmail.com>
2021-11-01 22:11:30 -0500

    navbar header added

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,63 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './Navbar';
+import Header from './Header';
+import Particles from 'react-particles-js';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+    particlesCanva: {
+        position:"absolute"
+    }
+})
 
 export const Home = () => {
+
+    const classes = useStyles()
     return (
-        <div>
-            <h2>This is a Home component</h2>
-        </div>
+        <>
+            <Particles 
+                canvasClassName={classes.particlesCanva}
+                params={{
+                    particles: {
+                        number: {
+                            value: 45,
+                            density: {
+                              enable: true,
+                              value_area: 900
+                            }
+                        },
+                        shape: {
+                          type: "circle",
+                          strole: {
+                            width: 1,
+                            color: "tomato"
+                          }
+                        },
+                        size: {
+                          value: 5,
+                          random: true,
+                          anim: {
+                            enable: true,
+                            speed: 5,
+                            size_min: 0.1,
+                            sync: true
+                          }
+                        },
+                        // opacity: {
+                        //   random: true,
+                        //   anim: {
+                        //     enable: true,
+                        //     speed: 1,
+                        //     opacity_min: 0.1,
+                        //     sync: true
+                        //   }
+                        // }
+                    }
+                }}
+            />
+            <Navbar/>
+            <Header/>
+        </>
     )
 }
 
